c3d302bf774ff90d37271fc8263c5a1866dad728
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-11-05 18:39:20 +0100

    add funzione e bottone

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 
 
 let emailList = []
+listaEmail = document.createElement ('ul')
 
-for (i = 0; i < 10; i++) {
-  axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((res) => {
-
-      const email = res.data.response
-      emailList.push(email)
-      console.log(emailList)
-    })
+const btn = document.getElementById ('button')
+console.log (btn)
+btn.addEventListener ('click', function() {
+  emailList.length = 0;
+  generaEmailRandom (10)
   
+})
 
-    .catch((err) => {
-      console.log('Qualcosa è andato storto', err)
 
-    })
+
+function generaEmailRandom (num) {
+   listaEmail.textContent = ''
+  for (i = 0; i < num; i++) {
+    
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((res) => {
+      
+        const email = res.data.response
+        emailList.push(email)
+        console.log(emailList)
+        const lista = document.createElement ('li')  
+        lista.textContent = email
+        document.body.appendChild (listaEmail)
+        listaEmail.appendChild (lista)
+      })
+      
+  
+  
+      .catch((err) => {
+        console.log('Qualcosa è andato storto', err)
+  
+      })
+  }
+  
 }
+
+
+
 
 
